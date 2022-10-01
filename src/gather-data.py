ba895d683ba332d0686d853cb7645e32dd5ff448
Lekima Yakuden <lekimoonya@gmail.com>
2022-10-01 15:35:42 -0400

    Added get_image_info function to grab data from Google EE image and return the features in a pandas df

---
+++ src/gather-data.py
@@ -0,0 +1,87 @@
+# ---
+# jupyter:
+#   jupytext:
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: 'Python 3.9.13 (''env'': venv)'
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Read Between the Wines 
+#
+# ## Imports
+
+# %%
+import ee # import the earth engine api 
+import datetime
+import folium
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# %% [markdown]
+# ## Retrieve the data
+#
+# ### Download the Google Earth Engine data
+
+# %%
+# authenticate & init google earth engine
+ee.Authenticate()
+ee.Initialize()
+
+# %%
+import geemap
+
+img_collection = ee.ImageCollection("ECMWF/ERA5_LAND/MONTHLY")
+
+def get_image_info(lat, lon, year):
+
+    # Filtering dates by northern and southern hemispheres
+    if lat > 0: # northern hemisphere
+        start_date = f'{year}-04-01'
+        end_date = f'{year}-10-31'
+    else: # southern hemisphere
+        start_date = f'{year - 1}-10-31'
+        end_date = f'{year}-04-01'
+
+    # Collection unique to the year
+    func_collection = img_collection.select('skin_temperature', 'dewpoint_temperature_2m', 'soil_temperature_level_1', 
+                                            'volumetric_soil_water_layer_1', 'surface_pressure', 'total_precipitation', 
+                                            'u_component_of_wind_10m', 'v_component_of_wind_10m').filterDate(start_date, end_date)
+
+    # Point of interest in lon/lat
+    location = ee.Geometry.Point(lon, lat)
+
+    # Load data into numpy array
+    data = np.asarray(func_collection.getRegion(location, 1000).getInfo())
+
+    # Convert all rows past the first to floats
+    header = data[0][1:]
+    data = data[1:, 1:].astype(float)
+
+    # Transpose the data and take the mean of each row
+    data = np.mean(data.T, axis=1)
+
+    # Combine the wind speeds into one column
+    data = np.insert(data, 11, np.sqrt(data[9]**2 + data[10]**2), axis=0)
+    header = np.insert(header, 11, 'wind_speed', axis=0)
+
+    # Remove lat/lon, time, and wind components
+    data = np.delete(data, [0, 1, 2, 9, 10], axis=0)
+    header = np.delete(header, [0, 1, 2, 9, 10], axis=0)
+
+    # Convert the data to a pandas dataframe
+    df = pd.DataFrame(data=data, index=header).T
+
+    # Return means of each column 
+    return df
+
+
+
