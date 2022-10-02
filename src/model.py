a94cf0ab880717745d7eadaaf21df727fa9dc9d2
Nathalie Redick <>
2022-10-01 22:18:29 -0400

    wine data with header

--- src/model.py
+++ src/model.py
@@ -24,38 +24,6 @@ from keras.models import Sequential
 from keras.layers import Dense
 import pandas as pd
 
-
-# %%
-# get the model
-def get_model(n_inputs, n_outputs):
-	model = Sequential()
-	model.add(Dense(20, input_dim=n_inputs, kernel_initializer='he_uniform', activation='relu'))
-	model.add(Dense(n_outputs))
-	model.compile(loss='mae', optimizer='adam')
-	return model
-
-# evaluate a model using repeated k-fold cross-validation
-def evaluate_model(X, y):
-	results = list()
-	n_inputs, n_outputs = X.shape[1], y.shape[1]
-	# define evaluation procedure
-	cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
-	# enumerate folds
-	for train_ix, test_ix in cv.split(X):
-		# prepare data
-		X_train, X_test = X[train_ix], X[test_ix]
-		y_train, y_test = y[train_ix], y[test_ix]
-		# define model
-		model = get_model(n_inputs, n_outputs)
-		# fit model
-		model.fit(X_train, y_train, verbose=0, epochs=100)
-		# evaluate model on test set
-		mae = model.evaluate(X_test, y_test, verbose=0)
-		# store result
-		print('>%.3f' % mae)
-		results.append(mae)
-	return results
-
 # %% [markdown]
 # # Load data
 #
