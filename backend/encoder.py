a4c79ea44bb901685a01e5904bf17dc6d078748f
Yasha <yashar@rezilion.com>
2022-10-02 02:17:46 -0400

    Create encoder skeleton

---
+++ backend/encoder.py
@@ -0,0 +1,14 @@
+
+
+class Encoder:
+    def __init__(self):
+        pass
+
+    def encode_features(self, year, winery):
+        pass
+
+    def _geoencode(self, winery):
+        pass
+
+    def _create_features(self, year, geoencoding):
+        pass
